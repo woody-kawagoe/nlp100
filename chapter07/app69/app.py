515bb8d3368ec19219446df2104283c2924678c4
koda-0303 <hip.hop.zip.please@gmail.com>
2017-05-18 18:53:34 +0900

    create app69

---
+++ chapter07/app69/app.py
@@ -0,0 +1,26 @@
+﻿#!/user/bin/env python
+# -*- coding: utf-8 -*-
+from bottle import (
+    route,
+    run,
+    template,
+    request,
+    static_file,
+    url,
+    get,
+    post,
+    response,
+    view,
+    error
+)
+import sys, codecs
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+ 
+@route("/")
+def index(name="hoge"):
+    name = request.forms.get("name")
+    aliases_name = request.forms.get("aliases-name")
+    tag = request.forms.get("tag")
+    return template('index', name=name)
+ 
+run(host="localhost", port=8000, debug=True, reloader=True)
---
+++ chapter07/app69/index.tpl
@@ -0,0 +1,7 @@
+﻿<form action="/" method="get">
+    アーティスト名: <input name="name" type="text" /></br>
+    アーティストの別名: <input name="aliases-name" type="text" /></br>
+    タグ: <input name="tag" type="text" /></br>
+    <input value="送信", type="submit" />
+</form>
+<p>name:{{name}}</p>
