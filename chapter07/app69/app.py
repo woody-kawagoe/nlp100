add4e1ce8a9358682a343b99e9688d994e7ebdcb
koda-0303 <hip.hop.zip.please@gmail.com>
2017-05-19 12:02:00 +0900

    show search result on app69

--- chapter07/app69/app.py
+++ chapter07/app69/app.py
@@ -13,14 +13,31 @@ from bottle import (
     view,
     error
 )
+from pymongo import MongoClient
 import sys, codecs
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
- 
+
 @route("/")
 def index(name="hoge"):
-    name = request.forms.get("name")
-    aliases_name = request.forms.get("aliases-name")
-    tag = request.forms.get("tag")
-    return template('index', name=name)
- 
+    get_val = {
+        'name':request.query.name,
+        'aliases_name':request.query.aliases_name,
+        'tags_value':request.query.tags_value
+    }
+    get_val['result'] = search(get_key(get_val))
+    return template('index', get_val)
+def search(key):
+    if key:
+        client = MongoClient('localhost', 27017)
+        artists = client.nlp_artists.artists
+        return [x for x in artists.find(key)][:10]
+    else:
+        return []
+def get_key(get_val):
+    key = {}
+    if get_val['name']: key['name'] = get_val['name']
+    if get_val['aliases_name']: key['aliases.name'] = get_val['aliases_name']
+    if get_val['tags_value']: key['tags.value'] = get_val['tags_value']
+    return key
+
 run(host="localhost", port=8000, debug=True, reloader=True)
--- chapter07/app69/index.tpl
+++ chapter07/app69/index.tpl
@@ -1,7 +1,13 @@
 ﻿<form action="/" method="get">
     アーティスト名: <input name="name" type="text" /></br>
-    アーティストの別名: <input name="aliases-name" type="text" /></br>
-    タグ: <input name="tag" type="text" /></br>
+    アーティストの別名: <input name="aliases_name" type="text" /></br>
+    タグ: <input name="tags_value" type="text" /></br>
     <input value="送信", type="submit" />
 </form>
 <p>name:{{name}}</p>
+<p>aliases_name:{{aliases_name}}</p>
+<p>tags_value:{{tags_value}}</p>
+% for a in result:
+<h1>{{a['name']}}</h1>
+<p>{{a}}</p>
+% end
